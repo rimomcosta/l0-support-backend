2a66018a3294d0a42789e3f0a762ef7758d42d8f
Rimom Costa <rcosta@adobe.com>
2025-01-15 23:46:53 +0000

    chore: Add more allowed origins for development and testing

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,9 +1,12 @@
+import { logger } from '../services/logger.js';
 export const corsConfig = {
     origin: function (origin, callback) {
         const allowedOrigins = [
-            'http://localhost:3000',
-            process.env.CLIENT_ORIGIN,
-            process.env.REACT_APP_API_URL
+            'http://localhost:3000',          // Development
+            process.env.CLIENT_ORIGIN,       // Defined in .env
+            process.env.REACT_APP_API_URL,   // Defined in .env
+            'https://l0support.ngrok.io',    // Ngrok endpoint
+            'http://10.122.12.162:3000'      // Server's local address
         ].filter(Boolean);
 
         if (!origin || allowedOrigins.includes(origin)) {
